06052da5cd910136b5bf0235771d38228580d408
Prateek Saxena <prtksxna@gmail.com>
2018-12-31 08:08:05 +0530

    feat: add blockquote styling

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,700|IBM+Plex+Sans+Condensed:400,700|IBM+Plex+Serif:400,400i,700|IBM+Plex+Sans:100,400" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,700|IBM+Plex+Sans+Condensed:400,700|IBM+Plex+Serif:300i,400,400i,700|IBM+Plex+Sans:100,300i,400" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
