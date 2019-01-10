a4097f2f78bacff38dc15180177c5a37e4d64f1f
Prateek Saxena <prtksxna@gmail.com>
2019-01-10 09:41:35 +0530

    fix(comments): display styling

--- comments.php
+++ comments.php
@@ -26,28 +26,6 @@ if ( post_password_required() ) {
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) :
 		?>
-		<h2 class="comments-title">
-			<?php
-			$zuari_comment_count = get_comments_number();
-			if ( '1' === $zuari_comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'zuari' ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			} else {
-				printf( // WPCS: XSS OK.
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $zuari_comment_count, 'comments title', 'zuari' ) ),
-					number_format_i18n( $zuari_comment_count ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			}
-			?>
-		</h2><!-- .comments-title -->
-
-		<?php the_comments_navigation(); ?>
-
 		<ol class="comment-list">
 			<?php
 			wp_list_comments( array(
--- single.php
+++ single.php
@@ -19,13 +19,12 @@ get_header();
 
 			get_template_part( 'template-parts/content', zuari_post_type_discovery() );
 
-			the_post_navigation();
-
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
 			endif;
 
+			the_post_navigation();
 		endwhile; // End of the loop.
 		?>
 
