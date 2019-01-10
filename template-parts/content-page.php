f0271034d97df94d1589b790f87d9a587da4108d
Prateek Saxena <prtksxna@gmail.com>
2019-01-10 10:02:13 +0530

    feat(page): fix layout

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,43 +10,49 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+	<div class="entry-meta">
+		<!-- TODO: Do we want to show any meta on pages? -->
+		&nbsp;
+	</div>
+	<div class="entry">
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</header><!-- .entry-header -->
 
-	<?php zuari_post_thumbnail(); ?>
+		<?php zuari_post_thumbnail(); ?>
 
-	<div class="entry-content">
-		<?php
-		the_content();
-
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'zuari' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
+		<div class="entry-content">
 			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'zuari' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					get_the_title()
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
+			the_content();
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'zuari' ),
+				'after'  => '</div>',
+			) );
 			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
+		</div><!-- .entry-content -->
+
+		<?php if ( get_edit_post_link() ) : ?>
+			<div class="entry-footer">
+				<?php
+				edit_post_link(
+					sprintf(
+						wp_kses(
+							/* translators: %s: Name of current post. Only visible to screen readers */
+							__( 'Edit <span class="screen-reader-text">%s</span>', 'zuari' ),
+							array(
+								'span' => array(
+									'class' => array(),
+								),
+							)
+						),
+						get_the_title()
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+				?>
+			</div><!-- .entry-footer -->
+		<?php endif; ?>
+	</div><!-- .entry -->
 </article><!-- #post-<?php the_ID(); ?> -->
