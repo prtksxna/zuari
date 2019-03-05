9ea44846cfb75f262171aa927599b6e0bafd837b
Prateek Saxena <prtksxna@gmail.com>
2019-03-05 07:42:59 +0530

    chore: setup grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,12 +9,18 @@ module.exports = function(grunt) {
           'style.css': 'style.scss'
         }
       }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['watch']);
 
 };
