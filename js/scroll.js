b70bd10495de65cd1847e5814f859cb1cb02bca2
Prateek Saxena <prtksxna@gmail.com>
2019-03-25 07:51:03 +0530

    fix: scrolling behavior

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,8 @@
 (function ( $ ) {
   var $els = $('.content');
   var $window = $(window);
+
+  // Reveal content as the user scrolls
   $window.on('scroll resize', function () {
     var window_height = $window.height();
     var window_top = $window.scrollTop();
@@ -30,10 +32,15 @@
     })
   });
 
+  // Hide only the content that is out of view
+  var window_height = $window.height();
+  var window_top = $window.scrollTop();
+  var window_bottom = (window_top + window_height);
   $.each( $els, function () {
-    // Hide the content that is out of view
-    if ( $(this).offset().top > $window.height() ) {
-      $('.content').addClass('content_type_hidden');
+    var $el = $(this);
+    var el_top = $el.offset().top;
+    if ( el_top >= window_bottom )  {
+      $(this).addClass('content_type_hidden');
     }
   })
 } ( jQuery ) )
