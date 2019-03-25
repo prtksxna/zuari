29f4febf4dceb6c378ff557f02fd1dab5f5a10a4
Prateek Saxena <prtksxna@gmail.com>
2019-03-25 08:57:27 +0530

    feat: add support for new gutenberg alignments

--- functions.php
+++ functions.php
@@ -24,34 +24,13 @@ if ( ! function_exists( 'zuari_setup' ) ) :
 		 */
 		load_theme_textdomain( 'zuari', get_template_directory() . '/languages' );
 
-		// Add default posts and comments RSS feed links to head.
+
 		add_theme_support( 'automatic-feed-links' );
-
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
 		add_theme_support( 'title-tag' );
-
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
 		add_theme_support( 'post-thumbnails' );
 		add_theme_support( 'custom-background' );
-
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus( array(
-			'header-menu' => esc_html__( 'Primary', 'zuari' ),
-		) );
-
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
+		add_theme_support( 'customize-selective-refresh-widgets' );
+		add_theme_support( 'align-wide' );
 		add_theme_support( 'html5', array(
 			'search-form',
 			'comment-form',
@@ -59,20 +38,15 @@ if ( ! function_exists( 'zuari_setup' ) ) :
 			'gallery',
 			'caption',
 		) );
-
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
-
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
 		add_theme_support( 'custom-logo', array(
 			'height'      => 100,
 			'width'       => 100,
 			'flex-width'  => true,
 			'flex-height' => false,
+		) );
+
+		register_nav_menus( array(
+			'header-menu' => esc_html__( 'Primary', 'zuari' ),
 		) );
 	}
 endif;
