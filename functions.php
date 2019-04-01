2dec2ecfb0a3ae10764170f045fb246970b5001a
Prateek Saxena <prtksxna@gmail.com>
2019-04-01 07:22:05 +0530

    fix(nav): normalize styles for <ul> too

--- functions.php
+++ functions.php
@@ -133,18 +133,18 @@ add_action( 'after_setup_theme', 'zuari_content_width', 0 );
  */
 function zuari_widgets_init() {
 	register_sidebar( array(
-		'name'          => esc_html__( 'Intro', 'zuari' ),
-		'id'            => 'sidebar-intro',
-		'description'   => esc_html__( 'Widgets for the intro section.', 'zuari' ),
+		'name'          => esc_html__( 'Footer', 'zuari' ),
+		'id'            => 'sidebar-footer',
+		'description'   => esc_html__( 'Widgets for the footer section.', 'zuari' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
 	register_sidebar( array(
-		'name'          => esc_html__( 'Footer', 'zuari' ),
-		'id'            => 'sidebar-footer',
-		'description'   => esc_html__( 'Widgets for the footer section.', 'zuari' ),
+		'name'          => esc_html__( 'Intro', 'zuari' ),
+		'id'            => 'sidebar-intro',
+		'description'   => esc_html__( 'Widgets for the intro section.', 'zuari' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
 		'before_title'  => '<h2 class="widget-title">',
