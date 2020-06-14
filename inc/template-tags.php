prtksxna/zuari

commit a10eb47e1f9a01b07b22c45e1a167f2cfc749833

refactor: fix phpcs issues #39

inc/template-tags.php
@@ -14,7 +14,8 @@ if ( ! function_exists( 'zuari_posted_on' ) ) :
 	function zuari_posted_on() {
 		$time_string = '<time class="entry-date published updated dt-published" datetime="%1$s">%2$s</time>';
 
-		$time_string = sprintf( $time_string,
+		$time_string = sprintf(
+			$time_string,
 			esc_attr( get_the_date( DATE_W3C ) ),
 			esc_html( get_the_date() ),
 			esc_attr( get_the_modified_date( DATE_W3C ) ),
@@ -25,7 +26,7 @@ if ( ! function_exists( 'zuari_posted_on' ) ) :
 			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark" class="u-url">' . $time_string . '</a>'
 		);
 
-		echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
+		echo '<span class="posted-on">' . $posted_on . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 
 	}
 endif;
@@ -39,7 +40,7 @@ if ( ! function_exists( 'zuari_posted_by' ) ) :
 			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 		);
 
-		echo '<span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+		echo '<span class="byline"> ' . $byline . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 
 	}
 endif;
@@ -52,26 +53,32 @@ if ( ! function_exists( 'zuari_entry_footer' ) ) :
 		// Hide tag text for pages.
 		if ( 'post' === get_post_type() ) {
 			echo '<span class="content__meta__tags">';
-			if (get_the_tags()) {
-				foreach (get_the_tags() as $tag) {
-					echo "<a rel=\"tag\" class=\"p-category\" href=\"";
-					echo esc_url(get_tag_link($tag->term_id));
-					echo "\">".$tag->name."</a>\n";
+			if ( get_the_tags() ) {
+				foreach ( get_the_tags() as $tag ) {
+					echo '<a rel=\"tag\" class=\"p-category\" href=\"';
+					echo esc_url( get_tag_link( $tag->term_id ) );
+					echo '\">' . $tag->name . '</a>\n'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 				}
 			}
 			echo '</span>';
 		}
 
-		/*if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+		/*
+		Commonting out old code.
+		if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 			echo '<span class="comments-link">';
 			comments_popup_link( '' );
 			echo '</span>';
-		}*/
+		}
+		*/
 
 		if ( function_exists( 'get_syndication_links' ) ) {
-			echo get_syndication_links( null, array(
-				'show_text_before' => null
-			) );
+			echo get_syndication_links( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+				null,
+				array(
+					'show_text_before' => null,
+				)
+			);
 		}
 
 		edit_post_link(
@@ -109,25 +116,35 @@ if ( ! function_exists( 'zuari_post_thumbnail' ) ) :
 			?>
 
 			<div class="content__thumbnail">
-				<?php the_post_thumbnail('large', array(
-					'class' => 'u-photo'
-				)); ?>
+				<?php
+				the_post_thumbnail(
+					'large',
+					array(
+						'class' => 'u-photo',
+					)
+				);
+				?>
 			</div><!-- .post-thumbnail -->
 
 		<?php else : ?>
 
 		<a class="content__thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
 			<?php
-			the_post_thumbnail( 'large', array(
-				'class' => 'u-photo',
-				'alt' => the_title_attribute( array(
-					'echo' => false,
-				) ),
-			) );
+			the_post_thumbnail(
+				'large',
+				array(
+					'class' => 'u-photo',
+					'alt'   => the_title_attribute(
+						array(
+							'echo' => false,
+						)
+					),
+				)
+			);
 			?>
 		</a>
 
-		<?php
+			<?php
 		endif; // End is_singular().
 	}
 endif;

inc/custom-header.php
@@ -13,13 +13,19 @@
  * @uses zuari_header_style()
  */
 function zuari_custom_header_setup() {
-	add_theme_support( 'custom-header', apply_filters( 'zuari_custom_header_args', array(
-		'default-image'          => '',
-		'default-text-color'     => '000000',
-		'flex-width'             => true,
-		'flex-height'            => true,
-		'wp-head-callback'       => 'zuari_header_style',
-	) ) );
+	add_theme_support(
+		'custom-header',
+		apply_filters(
+			'zuari_custom_header_args',
+			array(
+				'default-image'      => '',
+				'default-text-color' => '000000',
+				'flex-width'         => true,
+				'flex-height'        => true,
+				'wp-head-callback'   => 'zuari_header_style',
+			)
+		)
+	);
 }
 add_action( 'after_setup_theme', 'zuari_custom_header_setup' );
 
@@ -51,10 +57,10 @@ if ( ! function_exists( 'zuari_header_style' ) ) :
 				position: absolute;
 				clip: rect(1px, 1px, 1px, 1px);
 			}
-		<?php
-		// If the user has set a custom color for the text use that.
-		else :
-			?>
+			<?php
+			// If the user has set a custom color for the text use that.
+			else :
+				?>
 			.header a,
 			.header {
 				color: #<?php echo esc_attr( $header_text_color ); ?>;

inc/customizer.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * zuari Theme Customizer
+ * Zuari Theme Customizer
  *
  * @package zuari
  */
@@ -11,69 +11,93 @@
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
 function zuari_customize_register( $wp_customize ) {
-	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
-	$wp_customize->get_setting( 'header_image' )->transport = 'postMessage';
-	$wp_customize->get_setting( 'header_image_data'  )->transport = 'postMessage';
+	$wp_customize->get_setting( 'blogname' )->transport          = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport   = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_image' )->transport      = 'postMessage';
+	$wp_customize->get_setting( 'header_image_data' )->transport = 'postMessage';
 
-
-	$wp_customize->add_setting('header_bgcolor', array(
-		'default' => '#eaeaea',
-		'transport' => 'postMessage',
-		'sanitize_callback' => 'sanitize_hex_color',
-	) );
-	$wp_customize->add_control( new WP_Customize_Color_Control(
-		$wp_customize,
+	$wp_customize->add_setting(
 		'header_bgcolor',
 		array(
-			'label'      => __( 'Header Background Color', 'zuari' ),
-			'section'    => 'colors',
-			'settings'   => 'header_bgcolor',
-	    'priority'   => 1
+			'default'           => '#eaeaea',
+			'transport'         => 'postMessage',
+			'sanitize_callback' => 'sanitize_hex_color',
 		)
-	));
+	);
 
-	$wp_customize->add_setting('fgcolor', array(
-		'default' => '#000000',
-		'transport' => 'postMessage',
-		'sanitize_callback' => 'sanitize_hex_color',
-	) );
-	$wp_customize->add_control( new WP_Customize_Color_Control(
-		$wp_customize,
+	$wp_customize->add_control(
+		new WP_Customize_Color_Control(
+			$wp_customize,
+			'header_bgcolor',
+			array(
+				'label'    => __( 'Header Background Color', 'zuari' ),
+				'section'  => 'colors',
+				'settings' => 'header_bgcolor',
+				'priority' => 1,
+			)
+		)
+	);
+
+	$wp_customize->add_setting(
 		'fgcolor',
 		array(
-			'label'      => __( 'Foreground Color', 'zuari' ),
-			'section'    => 'colors',
-			'settings'   => 'fgcolor',
-	    #'priority'   => 5
+			'default'           => '#000000',
+			'transport'         => 'postMessage',
+			'sanitize_callback' => 'sanitize_hex_color',
 		)
-	));
+	);
 
-	$wp_customize->add_setting( 'enable_darkmode', array (
-		'default' => '',
-		'sanitize_callback' => 'sanitize_enable_darkmode'
-	) );
-	$wp_customize->add_control( new WP_CUstomize_Control(
-		$wp_customize,
+	$wp_customize->add_control(
+		new WP_Customize_Color_Control(
+			$wp_customize,
+			'fgcolor',
+			array(
+				'label'    => __( 'Foreground Color', 'zuari' ),
+				'section'  => 'colors',
+				'settings' => 'fgcolor',
+				// 'priority'   => 5
+			)
+		)
+	);
+
+	$wp_customize->add_setting(
 		'enable_darkmode',
 		array(
-			'label' => __( 'Allow the operating system\'s dark mode to override my color settings.', 'zuari' ),
-			'section' => 'colors',
-			'settings' => 'enable_darkmode',
-			'type' => 'checkbox'
+			'default'           => '',
+			'sanitize_callback' => 'zuari_sanitize_enable_darkmode',
 		)
-	) );
+	);
+
+	$wp_customize->add_control(
+		new WP_CUstomize_Control(
+			$wp_customize,
+			'enable_darkmode',
+			array(
+				'label'    => __( 'Allow the operating system\'s dark mode to override my color settings.', 'zuari' ),
+				'section'  => 'colors',
+				'settings' => 'enable_darkmode',
+				'type'     => 'checkbox',
+			)
+		)
+	);
 
 	if ( isset( $wp_customize->selective_refresh ) ) {
-		$wp_customize->selective_refresh->add_partial( 'blogname', array(
-			'selector'        => '.header__title a',
-			'render_callback' => 'zuari_customize_partial_blogname',
-		) );
-		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
-			'selector'        => '.header__description',
-			'render_callback' => 'zuari_customize_partial_blogdescription',
-		) );
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.header__title a',
+				'render_callback' => 'zuari_customize_partial_blogname',
+			)
+		);
+
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.header__description',
+				'render_callback' => 'zuari_customize_partial_blogdescription',
+			)
+		);
 	}
 }
 add_action( 'customize_register', 'zuari_customize_register' );
@@ -100,7 +124,7 @@ function zuari_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function zuari_customize_preview_js() {
-	wp_enqueue_script( 'zuari-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), false, true );
+	wp_enqueue_script( 'zuari-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), 'zuari', true );
 }
 add_action( 'customize_preview_init', 'zuari_customize_preview_js' );
 
@@ -113,12 +137,12 @@ function zuari_header_bgcolor_css() {
 	?>
 	<style media="screen">
 		.header {
-			background-color: <?php echo esc_attr(get_theme_mod('header_bgcolor', 'eaeaea')); ?>;
+			background-color: <?php echo esc_attr( get_theme_mod( 'header_bgcolor', 'eaeaea' ) ); ?>;
 		}
 	</style>
-<?php
+	<?php
 }
-add_action( 'wp_head', 'zuari_header_bgcolor_css');
+add_action( 'wp_head', 'zuari_header_bgcolor_css' );
 
 /**
  * Render the text and links in the correct color.
@@ -129,12 +153,12 @@ function zuari_fgcolor_css() {
 	?>
 	<style media="screen">
 		:root {
-			--fg-color: <?php echo esc_attr(get_theme_mod('fgcolor', '#000000')); ?>;
+			--fg-color: <?php echo esc_attr( get_theme_mod( 'fgcolor', '#000000' ) ); ?>;
 		}
 	</style>
-<?php
+	<?php
 }
-add_action( 'wp_head', 'zuari_fgcolor_css');
+add_action( 'wp_head', 'zuari_fgcolor_css' );
 
 /**
  * Override the colors and add the dark mode CSS class if enabled
@@ -142,8 +166,7 @@ add_action( 'wp_head', 'zuari_fgcolor_css');
  * @return void
  */
 function zuari_dark_mode() {
-	if ( get_theme_mod( 'enable_darkmode') === '1' ) {
-
+	if ( get_theme_mod( 'enable_darkmode' ) === '1' ) {
 		?>
 			<style media="screen">
 			@media (prefers-color-scheme: dark) {
@@ -160,10 +183,16 @@ function zuari_dark_mode() {
 		<?php
 	}
 }
-add_action( 'wp_head', 'zuari_dark_mode');
+add_action( 'wp_head', 'zuari_dark_mode' );
 
-function sanitize_enable_darkmode( $input ) {
-	if ( $input === true || $input === '1' ) {
+/**
+ * Takes the input value and turns it into something we can use reliably.
+ *
+ * @param boolean $input This is sometimes a boolean and sometimes a string.
+ * @return String
+ */
+function zuari_sanitize_enable_darkmode( $input ) {
+	if ( true === $input || '1' === $input ) {
 		return '1';
 	}
 	return '';

inc/jetpack.php
@@ -16,31 +16,37 @@
  */
 function zuari_jetpack_setup() {
 	// Add theme support for Infinite Scroll.
-	add_theme_support( 'infinite-scroll', array(
-		'container' => 'main',
-		'render'    => 'zuari_infinite_scroll_render',
-		'footer'    => 'page',
-	) );
+	add_theme_support(
+		'infinite-scroll',
+		array(
+			'container' => 'main',
+			'render'    => 'zuari_infinite_scroll_render',
+			'footer'    => 'page',
+		)
+	);
 
 	// Add theme support for Responsive Videos.
 	add_theme_support( 'jetpack-responsive-videos' );
 
 	// Add theme support for Content Options.
-	add_theme_support( 'jetpack-content-options', array(
-		'post-details'    => array(
-			'stylesheet' => 'zuari-style',
-			'date'       => '.posted-on',
-			'categories' => '.cat-links',
-			'tags'       => '.tags-links',
-			'author'     => '.byline',
-			'comment'    => '.comments-link',
-		),
-		'featured-images' => array(
-			'archive'    => true,
-			'post'       => true,
-			'page'       => true,
-		),
-	) );
+	add_theme_support(
+		'jetpack-content-options',
+		array(
+			'post-details'    => array(
+				'stylesheet' => 'zuari-style',
+				'date'       => '.posted-on',
+				'categories' => '.cat-links',
+				'tags'       => '.tags-links',
+				'author'     => '.byline',
+				'comment'    => '.comments-link',
+			),
+			'featured-images' => array(
+				'archive' => true,
+				'post'    => true,
+				'page'    => true,
+			),
+		)
+	);
 }
 add_action( 'after_setup_theme', 'zuari_jetpack_setup' );
 

inc/template-functions.php
@@ -47,7 +47,7 @@ add_action( 'wp_head', 'zuari_pingback_header' );
 function zuari_post_type_discovery() {
 	if (
 			has_post_thumbnail() &&
-			get_the_title() === ""
+			get_the_title() === ''
 	) {
 		return 'image';
 	}
@@ -69,8 +69,16 @@ function zuari_post_type_discovery() {
 	return get_post_type();
 }
 
+/**
+ * Detect what type of blog post this is. The return values should match
+ * get_post_format values where possible
+ *
+ * @link https://developer.wordpress.org/reference/functions/get_the_archive_title/
+ * @param String $title Archive title.
+ * @return String
+ */
 function zuari_archive_title( $title ) {
-	// From https://www.binarymoon.co.uk/2017/02/hide-archive-title-prefix-wordpress/
+	// From https://www.binarymoon.co.uk/2017/02/hide-archive-title-prefix-wordpress/.
 
 	// Skip if the site isn't LTR, this is visual, not functional.
 	// Should try to work out an elegant solution that works for both directions.

phpcs.xml.dist
@@ -75,7 +75,7 @@
 	<rule ref="WordPress.WP.I18n">
 		<properties>
 			<property name="text_domain" type="array">
-				<element value="my-textdomain"/>
+				<element value="zuari"/>
 				<element value="library-textdomain"/>
 			</property>
 		</properties>
@@ -84,7 +84,7 @@
 	<rule ref="WordPress.NamingConventions.PrefixAllGlobals">
 		<properties>
 			<property name="prefixes" type="array">
-				<element value="my_prefix"/>
+				<element value="zuari"/>
 			</property>
 		</properties>
 	</rule>

template-parts/content-aside.php
@@ -8,7 +8,7 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('content h-entry'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'content h-entry' ); ?>>
 	<div class="content__meta">
 		<?php
 		zuari_posted_on();
@@ -18,23 +18,27 @@
 
 	<div class="content__body content__body_type_aside e-content">
 		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'zuari' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
+		the_content(
+			sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'zuari' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			)
+		);
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'zuari' ),
-			'after'  => '</div>',
-		) );
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'zuari' ),
+				'after'  => '</div>',
+			)
+		);
 		?>
 	</div><!-- .content__body -->
 </article><!-- .content -->

template-parts/content-image.php
@@ -1,14 +1,14 @@
 <?php
 /**
-* Template part for displaying image posts
-*
-* @link https://developer.wordpress.org/themes/basics/template-hierarchy/
-* @package zuari
-*/
+ * Template part for displaying image posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * @package zuari
+ */
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('content h-entry'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'content h-entry' ); ?>>
 	<div class="content__meta">
 		<?php
 		zuari_posted_on();
@@ -20,23 +20,27 @@
 
 	<div class="content__body content__body_type_aside e-content">
 		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'zuari' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-			) );
+		the_content(
+			sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'zuari' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			)
+		);
 
-			wp_link_pages( array(
+		wp_link_pages(
+			array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'zuari' ),
 				'after'  => '</div>',
-			) );
+			)
+		);
 		?>
-	</div><!-- .content__body -->
-</article><!-- .content -->
+		</div><!-- .content__body -->
+	</article><!-- .content -->

commit 63b2479758ed8151ccff086f542c135c1bf4159b

fix: tag urls in zuari_entry_footer
This was a regression caused in:
a10eb47e1f9a01b07b22c45e1a167f2cfc749833

When switching from double to single quotes I continued to escape
the quotes. This was not required and was creating strange URLs

https://stackoverflow.com/a/3446286/366138
https://www.php.net/manual/en/language.types.string.php#language.types.string.syntax.single

inc/template-tags.php
@@ -55,9 +55,9 @@ if ( ! function_exists( 'zuari_entry_footer' ) ) :
 			echo '<span class="content__meta__tags">';
 			if ( get_the_tags() ) {
 				foreach ( get_the_tags() as $tag ) {
-					echo '<a rel=\"tag\" class=\"p-category\" href=\"';
+					echo '<a rel="tag" class="p-category" href="';
 					echo esc_url( get_tag_link( $tag->term_id ) );
-					echo '\">' . $tag->name . '</a>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+					echo '">' . $tag->name . '</a>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 				}
 			}
 			echo '</span>';